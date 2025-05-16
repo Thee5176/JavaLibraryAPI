43f0a13905824832bd24f6b7fc7c8461ad85ccc2
Thee5176 <theerapong5176@gmail.com>
2025-05-16 16:00:11 +0900

    Log File Analyzer

---
+++ src/main/java/com/thee5176/part11_file/logfile/LogFileAnalyzer.java
@@ -0,0 +1,85 @@
+package com.thee5176.part11_file.logfile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+public class LogFileAnalyzer {
+    private BufferedReader bfReader;
+
+    private static final String TIMESTAMP_PATTERN = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}";
+    private static final Pattern ERROR_LOG_PATTERN = Pattern.compile(
+            "(" + TIMESTAMP_PATTERN + ").*\\[ERROR] (.*)"
+    );
+
+    public LogFileAnalyzer(){}
+
+
+    //Getter
+    public BufferedReader getBfReader() {
+        return bfReader;
+    }
+
+    //Method
+    public List<String> analyzeLog(String file) {
+        //完成のデータ：Map<>
+        //#1:ファイルを開く
+        try {
+            bfReader = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: ファイルを開けない。");
+        }
+
+        //#2:ログファイルを1行ごとに読み込み
+        List<String> reportList = new ArrayList<>();
+        try {
+            String logLine;
+
+            while( (logLine = bfReader.readLine()) != null){
+                Matcher errorLog = ERROR_LOG_PATTERN.matcher(logLine);
+                if ( errorLog.matches() ){
+                    String timestamp = errorLog.group(1);
+                    String message = errorLog.group(2);
+                    reportList.add(timestamp + " - " + message);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error: ファイルを読めない。");
+        }
+
+        return reportList;
+    }
+
+    public void writeReport(Path outputfile, List<String> content) {
+        try {
+            Files.write(outputfile, content);
+            System.out.println("エラーレポートファイルを作成しました：error_report.txt");
+        } catch (IOException e) {
+            System.out.println("Error: ファイルを作成できません。。");
+        }
+    }
+
+    public static void main(String[] args) {
+        Path outputFile = Paths.get("error_report.txt");
+        String myLogFile = "src/main/resources/logfile.txt";
+
+        LogFileAnalyzer analyzer = new LogFileAnalyzer();
+        List<String> logList = analyzer.analyzeLog(myLogFile);
+
+        System.out.println("エラー状態：");
+        if ( !logList.isEmpty() ) {
+            for (String error : logList) {
+                analyzer.writeReport(outputFile, logList);
+                System.out.println(error);
+            }
+        } else {
+            System.out.println("エラーはありません。お疲れ様です。");
+        }
+    }
+}
